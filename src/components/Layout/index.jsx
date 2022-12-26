d11f26f68102f04752edb50e76de599a6f9b1bf8
Glenn Larsen <glenn_lar@hotmail.com>
2022-12-26 11:00:07 +0100

    mobile burger menu

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,22 +1,32 @@
-import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import { stack as Menu } from "react-burger-menu";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Tooltip from "@mui/material/Tooltip";
 
 function Layout({ children, showBackButton, backPage, showHowItWorks }) {
+  const [menuState, setMenuState] = useState(false);
   const navigate = useNavigate();
+
+  const closeMenu = () => {
+    setMenuState(false);
+  };
+
   const logout = () => {
     navigate("/");
+    closeMenu();
   };
 
   const goBack = () => {
     navigate(backPage ? `/${backPage}` : -1);
+    closeMenu();
   };
 
   return (
     <div className="main-layout">
-      <div>
+      <Menu width={310} right isOpen={menuState}>
+        <h2>Menu</h2>
         <Tooltip title="Go back">
           <ArrowBackIcon
             className="btn-back"
@@ -24,9 +34,32 @@ function Layout({ children, showBackButton, backPage, showHowItWorks }) {
             style={{ display: showBackButton }}
           ></ArrowBackIcon>
         </Tooltip>
-        <Link to="/howitworks" style={{ display: showHowItWorks }}>
+        <NavLink
+          to="/howitworks"
+          onClick={() => closeMenu()}
+          style={{ display: showHowItWorks }}
+        >
           How it Works?
-        </Link>
+        </NavLink>
+        <span>
+          <strong>User:</strong>
+        </span>
+        <address>flesland@sixt.no</address>
+        <button onClick={logout} className="btn-menu__mobile">
+          Logout
+        </button>
+      </Menu>
+      <nav className="desktop-nav">
+        <Tooltip title="Go back">
+          <ArrowBackIcon
+            className="btn-back"
+            onClick={goBack}
+            style={{ display: showBackButton }}
+          ></ArrowBackIcon>
+        </Tooltip>
+        <NavLink to="/howitworks" style={{ display: showHowItWorks }}>
+          How it Works?
+        </NavLink>
         <span>
           <strong>User:</strong>
         </span>
@@ -34,7 +67,7 @@ function Layout({ children, showBackButton, backPage, showHowItWorks }) {
         <button onClick={logout} className="btn-menu">
           Logout
         </button>
-      </div>
+      </nav>
       {children}
     </div>
   );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,7 +7,10 @@ import Head from "components/Head";
 function Login() {
   return (
     <Container>
-      <Head page="Login" description="Login to start using Sixt Driver info Forms" />
+      <Head
+        page="Login"
+        description="Login to start using Sixt Driver info Forms"
+      />
       <Header title="Login" info="Driver Info Forms" />
       <LoginForm />
     </Container>
